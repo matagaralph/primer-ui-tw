ec69f3a65385993be31e9317d21ad6d50e794bdd
matagaralph <matagaralph@gmail.com>
2025-07-10 04:04:37 +0200

    wrap app in session provider

--- components/app-layout.tsx
+++ components/app-layout.tsx
@@ -3,14 +3,15 @@ import AppNavbar from './NavBar';
 import { QueryProvider } from './query-provider';
 import { redirect } from 'next/navigation';
 import { authOptions } from '@/lib/auth';
+import AuthProvider from './session-provider';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   const user = getCurrentUser();
   if (!user) redirect(authOptions.pages?.signIn || '/login');
   return (
-    <>
+    <AuthProvider>
       <AppNavbar />
       <QueryProvider>{children}</QueryProvider>
-    </>
+    </AuthProvider>
   );
 }
