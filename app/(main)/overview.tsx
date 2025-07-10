714472177110928f0f6374d831a79eff3daffcac
matagaralph <matagaralph@gmail.com>
2025-07-10 03:58:59 +0200

    lock pages

---
+++ app/(main)/overview.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { Badge } from '@/components/badge';
+import StatsCard from '@/components/card';
+import { formatDateYMD } from '@/lib';
+import smoastersApi from '@/lib/http';
+import { Column, DataTable, Table } from '@primer/react/experimental';
+import { useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const columns: Column<ShopwareShipments>[] = [
+  {
+    header: 'Order Number',
+    field: 'associatedOrderNumber',
+    rowHeader: true,
+    sortBy: 'alphanumeric',
+  },
+  {
+    header: 'Date Placed',
+    field: 'createdAt',
+    renderCell: (row) => formatDateYMD(row.createdAt),
+    sortBy: 'datetime',
+  },
+  {
+    header: 'Tracking Link',
+    field: 'trackingCode',
+    renderCell: (row) => {
+      return row.trackingLink ? (
+        <Link
+          target='_blank'
+          className='tw:text-[var(--fgColor-link)]'
+          href={row.trackingLink}
+        >
+          {row.trackingCode}
+        </Link>
+      ) : (
+        'N/A'
+      );
+    },
+  },
+  {
+    header: 'Shipping Method',
+    field: 'shippingMethod',
+  },
+  {
+    header: 'State',
+    field: 'orderState',
+    renderCell: (row) =>
+      row.orderState === 'completed' ? (
+        <Badge color='green'>{row.orderState}</Badge>
+      ) : row.orderState === 'in_progress' ? (
+        <Badge color='yellow'>{row.orderState}</Badge>
+      ) : (
+        <Badge color='red'>{row.orderState}</Badge>
+      ),
+  },
+  {
+    header: 'Latest Shipping Date',
+    field: 'shippingDateLatest',
+    renderCell: (row) => formatDateYMD(row.shippingDateLatest),
+  },
+];
+
+export function OverView() {
+  const {
+    data: shipments = [],
+    isError,
+    error,
+    isLoading,
+  } = useQuery<ShopwareShipments[], Error>({
+    queryKey: ['shops'],
+    queryFn: () =>
+      smoastersApi
+        .get<ShopwareShipments[]>('/fufillments/shopware')
+        .then((res) => res.data),
+  });
+
+  const pageSize = 15;
+  const [pageIndex, setPageIndex] = useState(0);
+  const start = pageIndex * pageSize;
+  const end = start + pageSize;
+  const rows: ShopwareShipments[] = shipments.slice(start, end);
+
+  if (isError) return <div>Error: {error.message}</div>;
+  return (
+    <div className=''>
+      <Table.Container>
+        {isLoading && (
+          <Table.Skeleton
+            aria-labelledby='shipments loading'
+            rows={15}
+            columns={columns}
+          />
+        )}
+        {shipments && (
+          <>
+            <DataTable data={rows} columns={columns} />
+            <Table.Pagination
+              aria-label='Pagination for shipments'
+              pageSize={pageSize}
+              totalCount={shipments.length}
+              onChange={({ pageIndex }) => {
+                setPageIndex(pageIndex);
+              }}
+            />
+          </>
+        )}
+      </Table.Container>
+    </div>
+  );
+}
--- components/app-layout.tsx
+++ components/app-layout.tsx
@@ -1,7 +1,12 @@
+import { getCurrentUser } from '@/lib/session';
 import AppNavbar from './NavBar';
 import { QueryProvider } from './query-provider';
+import { redirect } from 'next/navigation';
+import { authOptions } from '@/lib/auth';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
+  const user = getCurrentUser();
+  if (!user) redirect(authOptions.pages?.signIn || '/login');
   return (
     <>
       <AppNavbar />
